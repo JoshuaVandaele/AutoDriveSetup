a5e1ca62e7b4ebd1127e3583ccf2e22f85ea0960
Folfy Blue <folfyblu@gmail.com>
2022-03-11 23:23:46 +0100

    Git alias 'upstreamfetch'

--- scripts/ApplyUserSettings.bat
+++ scripts/ApplyUserSettings.bat
@@ -25,6 +25,7 @@ powercfg.exe /hibernate on
 
 ::Add git shortcuts and preferences
 git config --global alias.submit "!git add -A && git commit && git push" || REM Don't judge me I'm lazy ok
+git config --global alias.upstreamfetch "!git fetch upstream && git checkout master && git reset --hard upstream/master || ECHO.Add your upstream repo with git remote add upstream /url/to/original/repo"
 git config --global core.editor "'C:\Program Files\Sublime Text\subl.exe' -nw"
 
 ::Move settings files

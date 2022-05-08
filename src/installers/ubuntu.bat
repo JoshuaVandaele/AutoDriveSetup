0429da71b265f1277dffdbd3be64b9189abf5f1e
FolfyBlue <folfyblu@gmail.com>
2022-05-08 18:52:22 +0200

    Fix ubuntu installer

--- src/installers/ubuntu.bat
+++ src/installers/ubuntu.bat
@@ -1,6 +1,8 @@
 winget install %wingetargs% --id "Canonical.Ubuntu"
 call "%SetupFolder%src\tools\RefreshEnv.cmd"
-wsl --update || REM WSL->WSL2
+wsl --install
+wsl --update 2>nul|| REM WSL->WSL2
+call "%SetupFolder%src\tools\RefreshEnv.cmd"
 ubuntu install --root || REM Install ubuntu
 ubuntu run sudo apt-get update || REM Update ubuntu, because fresh installs aren't already fully updated for some reason
 echo y|ubuntu run sudo apt-get upgrade

727052b1a1196f229101342a00be430f8ed437da
FolfyBlue <folfyblu@gmail.com>
2022-05-08 20:24:58 +0200

    Ubuntu requires admin privilege

--- src/bat/RunOnce/finishUbuntuInstall.bat
+++ src/bat/RunOnce/finishUbuntuInstall.bat
@@ -1,5 +1,15 @@
+::Elevate to administrator privileges if you do not have them
+NET SESSION || REM Admin only command, if the error level is 0, it means we have admin privileges
+if %errorLevel% == 0 (
+  goto setup 
+) else ( 
+  echo Getting administrator privileges..
+  powershell -command "Start-Process %0 -Verb runas" || REM Restart current batch file as admin
+)
+exit
+
+:setup
 wsl --update 2>nul|| REM WSL->WSL2
-call "%SetupFolder%src\tools\RefreshEnv.cmd"
 ubuntu install --root || REM Install ubuntu
 ubuntu run sudo apt-get update || REM Update ubuntu, because fresh installs aren't already fully updated for some reason
 echo y|ubuntu run sudo apt-get upgrade

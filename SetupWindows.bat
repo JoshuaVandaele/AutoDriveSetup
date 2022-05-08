885d51dec7913e4a12c15499bceb4f3cc7d800b7
FolfyBlue <folfyblu@gmail.com>
2022-05-08 14:27:02 +0200

    Better admin rights check

--- SetupWindows.bat
+++ SetupWindows.bat
@@ -4,7 +4,7 @@ set SetupFolder=%~dp0
 echo %SetupFolder%
 
 ::Elevate to administrator privileges if you do not have them
-sfc /VERIFYFILE=c:\windows\system32\kernel32.dll >nul 2>&1 || REM Admin only command, if the error level is 0, it means we have admin privileges
+NET SESSION || REM Admin only command, if the error level is 0, it means we have admin privileges
 if %errorLevel% == 0 (
   goto setup 
 ) else ( 

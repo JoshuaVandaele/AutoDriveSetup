66b720e6819a5dd287027007b89e3fb50f449196
FolfyBlue <folfyblu@gmail.com>
2022-07-01 17:24:59 +0200

    Minor bug fix

--- src/bat/setup/CreateMklinks.bat
+++ src/bat/setup/CreateMklinks.bat
@@ -5,8 +5,8 @@ mklink /J "C:\Program Files\Epic Games" "D:\Games\EpicLibrary"
 mklink /J "C:\ProgramData\Epic" "D:\Games\EpicLibrary\Epic"
 
 ::tf2, not really mklinks but shut up
-set sourceDir=D:\Program Files (x86)\Steam\steamapps\common\Team Fortress 2\tf
-set tfDir=C:\Users\folfy\steam\steamapps\common\Team Fortress 2\tf
+set sourceDir="D:\Program Files (x86)\Steam\steamapps\common\Team Fortress 2\tf"
+set tfDir="C:\Users\folfy\steam\steamapps\common\Team Fortress 2\tf"
 
 mkdir %tfDir%
-xcopy /Y /V /E "%sourceDir%" "%tfDir%"
+xcopy /Y /V /E %sourceDir% %tfDir%

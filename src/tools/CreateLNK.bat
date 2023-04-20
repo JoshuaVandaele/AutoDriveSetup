c4b3de14963e8239122f424fdd7ba66dd231fc1c
Folfy Blue <folfyblu@gmail.com>
2023-04-20 13:04:13 +0200

    fix: CreateLNK not working if the SOURCE parameter contains quotes

--- src/tools/CreateLNK.bat
+++ src/tools/CreateLNK.bat
@@ -2,13 +2,16 @@
 :: Example: CALL CreateLNK.bat "Dolphin.exe" "%USERPROFILE%\Start Menu\Programs\Startup\Dolphin.lnk"
 :: Made by Folfy Blue
 
+SET SOURCE=%~1
+SET DESTINATION=%~2
+
 set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
 
 echo Set oWS = WScript.CreateObject("WScript.Shell") > %SCRIPT%
-echo sLinkFile = "%2" >> %SCRIPT%
+echo sLinkFile = "%DESTINATION%" >> %SCRIPT%
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
-echo oLink.TargetPath = "%1" >> %SCRIPT%
-echo oLink.IconLocation = "%1, 0" >> %SCRIPT%
+echo oLink.TargetPath = "%SOURCE%" >> %SCRIPT%
+echo oLink.IconLocation = "%SOURCE%, 0" >> %SCRIPT%
 echo oLink.WindowStyle = 1 >> %SCRIPT%
 echo oLink.WorkingDirectory = "%INSTALL_LOCATION%" >> %SCRIPT%
 echo oLink.Save >> %SCRIPT%
